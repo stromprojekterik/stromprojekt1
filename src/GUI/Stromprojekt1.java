cbc4faeecbcd0b440b17bcd8cf2f2b44c06a3591
Administrator <Administrator@popcon>
2012-11-09 11:31:25 +0100

    Test

--- src/GUI/Stromprojekt1.java
+++ src/GUI/Stromprojekt1.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package stromprojekt1;
+package GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/GUI/Stromprojekt1Controller.java
+++ src/GUI/Stromprojekt1Controller.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package stromprojekt1;
+package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
