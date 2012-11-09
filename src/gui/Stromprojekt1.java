fb96462bb6900f02be2e06239b8bd7feaef83301
Tim <stromprojekt.tim@gmail.com>
2012-11-09 14:52:39 +0100

    package ändern

--- src/gui/Stromprojekt1.java
+++ src/gui/Stromprojekt1.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI;
+package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
